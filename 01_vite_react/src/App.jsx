7491342c077f8e89db3e663dd784f634699e5c1c
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2025-01-26 16:34:08 +0530

    createElement in React commit

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,10 +1,10 @@
 
 import Chai from './chai'
 function App() {
- 
+  const username="chai aur code"
   return (
     <>
-        <h1>chai and coffee</h1>
+        <h1>chai and coffee {username}</h1>
         <Chai/>
         <p>hello i am ritu</p>
     </>
